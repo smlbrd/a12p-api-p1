9ad435c6aeadfd4fd9cbd0081053f4f2e20e4f94
Wren Hawthorne <wren.hawthorne@madetech.com>
2026-06-09 13:40:41 +0100

    add github workflow for code coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,15 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
     test: {
         coverage: {
-            provider: 'v8'
+            provider: 'v8',
+            reporter: ['text', 'json-summary', 'json'],
+            reportOnFailure: true,
+            thresholds: {
+                lines: 80,
+                branches: 80,
+                functions: 80,
+                statements: 80
+            },
         },
     },
 })
