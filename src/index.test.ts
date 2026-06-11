333137dfa42ae0ef0cf9df5da0eacb968860b69d
Wren Hawthorne <wren.hawthorne@madetech.com>
2026-06-11 11:28:02 +0100

    failing test for post /coins

--- src/index.test.ts
+++ src/index.test.ts
@@ -3,6 +3,7 @@ import app from "./index.ts";
 import { db } from "./db/db.ts";
 import { coins } from "./db/schema.ts";
 import { seedCoins, coinsData } from "./seeds/coins.ts"
+import { eq } from "drizzle-orm";
 
 describe("/health", () => {
     test("should return a 200 status", async () => {
@@ -12,7 +13,7 @@ describe("/health", () => {
     })
 })
 
-describe("/coins", () => {
+describe("GET /coins", () => {
     beforeEach(async () => {
         await db.delete(coins);
 
@@ -27,4 +28,38 @@ describe("/coins", () => {
         const data = await res.json();
         expect(data).toEqual(coinsData);
     })
+})
+
+describe("POST /coins", () => {
+    beforeEach(async () => {
+        await db.delete(coins)
+    })
+
+    test("should add a new coin", async () => {
+        const newCoin = { name: "Testing.. Testing.. 1, 2, 3", isCompleted: false }
+
+        const res = await app.request("/coins", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(newCoin)
+        });
+
+        expect(res.status).toBe(201)
+
+        const data = await res.json();
+        expect(data).toMatchObject({
+            id: expect.any(String),
+            ...newCoin
+        });
+
+        const dbCoin = await db
+            .select()
+            .from(coins)
+            .where(eq(coins.id, data.id))
+            .limit(1);
+
+        expect(dbCoin).toMatchObject(newCoin);
+    })
+
+    // validation - fails if no name in body
 })
